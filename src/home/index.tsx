435651b16d5148f36c9460258749dc08413153f5
msz <2473872761@qq.com>
2020-04-30 14:53:15 +0800

    添加路由

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class Home extends Component {
+export default class extends Component {
 	public render() {
 		return (
-			<View>
-				<Text>
-					如果你喜欢在Web上使用React，那你也肯定会喜欢React Native.
-				</Text>
-				<Text>
-					基本上就是用原生组件比如'View'和'Text'
-					来代替web组件'div'和'span'。
-				</Text>
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Home Screen</Text>
 			</View>
 		);
 	}
