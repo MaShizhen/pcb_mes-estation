59dae443029850f48b6200a91542cb9d2a96ee1c
闫晶晶wsl <1915574432@qq.com>
2020-05-20 16:39:01 +0800

    as

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -12,7 +12,7 @@ export default () => {
 					</View>
 				</View>
 				{/* 密码登录 */}
-				{/* <View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#fffdf5', alignItems: 'center', width: '30%' }}>
+				<View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#fffdf5', alignItems: 'center', width: '30%' }}>
 					<View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#fffdf5', alignItems: 'center', opacity: 1 }}>
 						<View style={{ flexDirection: 'row', width: 60, borderRightWidth: 1, borderRightColor: '#fffdf5', justifyContent: 'center' }}>
 							< Text style={{ fontSize: 18, color: '#fffdf5' }}>账号</Text>
@@ -20,8 +20,8 @@ export default () => {
 					</View>
 					<TextInput placeholder='请输入账号' secureTextEntry={true} underlineColorAndroid="transparent" style={styles.inputs} placeholderTextColor='#fffdf5'>
 					</TextInput>
-				</View> */}
-				{/* <View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#fffdf5', alignItems: 'center', width: '30%' }}>
+				</View>
+				<View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#fffdf5', alignItems: 'center', width: '30%' }}>
 					<View style={{ height: 70, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#ececec', alignItems: 'center' }}>
 						<View style={{ flexDirection: 'row', width: 60, borderRightWidth: 1, borderRightColor: '#ddd', justifyContent: 'center' }}>
 							< Text style={{ fontSize: 18, color: '#fffdf5' }}>密码</Text>
@@ -29,21 +29,21 @@ export default () => {
 					</View>
 					<TextInput placeholder='请输入密码' style={styles.inputs} placeholderTextColor='#fffdf5' secureTextEntry={true} underlineColorAndroid="transparent">
 					</TextInput>
-				</View> */}
-				{/* <View style={{ height: 70, width: '30%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#242c3a', marginTop: 50, opacity: 0.5, borderRadius: 35 }}>
 				</View>
-				<Text style={{ color: '#FFF', textAlign: 'center', fontSize: 18, position: 'relative', top: -45 }}>登录</Text> */}
-				{/*<View>
-					 <View style={{ borderWidth: 1, borderColor: '#fffdf5' }}></View>
+				<View style={{ height: 70, width: '30%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#242c3a', marginTop: 50, opacity: 0.5, borderRadius: 35 }}>
+				</View>
+				<Text style={{ color: '#FFF', textAlign: 'center', fontSize: 18, position: 'relative', top: -45 }}>登录</Text>
+				<View style={{}}>
+					<View style={{ borderWidth: 1, borderColor: '#fffdf5' }}></View>
 					<Text style={{ color: '#fffdf5' }}>切换登录方式</Text>
 					<View style={{ borderWidth: 1, borderColor: '#fffdf5' }}></View>
-				</View>*/}
-				{/* <View style={{ width: '30%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 40 }}>
+				</View>
+				<View style={{ width: '30%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 40 }}>
 					<Fdicon name='qia1' size={70} color='#242c3a'></Fdicon>
 					<Fdicon name='zhiwen-xianxing' size={67} color='#242c3a'></Fdicon>
 					<Fdicon name='id1' size={70} color='#242c3a'></Fdicon>
 					<Fdicon name='nfc-1' size={70} color='#242c3a'></Fdicon>
-				</View> */}
+				</View>
 				{/* ID登录 */}
 				{/* <View style={{ marginTop: 40 }}>
 					<Fdicon name='id1' size={100} color='#242c3a'></Fdicon>
@@ -57,7 +57,7 @@ export default () => {
 					<Fdicon name='zhiwen-xianxing' size={100} color='#242c3a'></Fdicon>
 					<Text style={{}}>请刷指纹进行登录</Text>
 				</View> */}
-				<View style={{ marginTop: 40 }}>
+				{/* <View style={{ marginTop: 40 }}>
 					<Fdicon name='nfc-1' size={100} color='#242c3a'></Fdicon>
 					<Text style={{}}>请使用NFC进行登录</Text>
 				</View>
@@ -65,7 +65,7 @@ export default () => {
 				</View>
 				<View style={{ position: 'relative', top: -45 }}>
 					<Text style={{ color: '#FFF', fontSize: 18 }}>密码登录</Text>
-				</View>
+				</View> */}
 			</View>
 		</ImageBackground >
 	);
