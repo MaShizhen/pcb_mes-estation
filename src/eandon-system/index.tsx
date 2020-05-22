fa97a8c05a0900b8c4508c07f34024de9f8d3aea
闫晶晶wsl <1915574432@qq.com>
2020-05-22 11:35:36 +0800

    安灯

--- src/eandon-system/index.tsx
+++ src/eandon-system/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 export default () => {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>安灯系统</Text>
+			<View style={{ flex: 0.3, borderWidth: 1 }}>
+				<View style={{ height: 45 }}><Text style={{ lineHeight: 45, textAlign: 'center' }}>报警代码列表</Text></View>
+				<View>
+					<View style={{ height: 35, borderWidth: 1, borderRadius: 10, borderColor: '#999', marginTop: 10 }}>
+						<Text style={{ lineHeight: 35, textAlign: 'center' }}>安灯报警代码</Text>
+					</View>
+				</View>
+			</View>
+			<View style={{ flex: 0.7, borderWidth: 1 }}>
+				<View style={{ height: 45 }}><Text style={{ lineHeight: 45, textAlign: 'center' }}>报警代码详情</Text></View>
+				<View>
+					<Image source={require('../../imgs/science5.png')} />
+					<Text>水泵是否正常</Text>
+				</View>
+			</View>
 		</View>
 	);
 }
