4205772d83b77dab23799bff0e9eeb11ba2bf09f
macbook <dohard@163.com>
2020-06-03 11:07:49 +0800

    fix

--- src/esop-system/index.tsx
+++ src/esop-system/index.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
-import { Alert, StyleSheet } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import Pdf from 'react-native-pdf';
-import uselocal from '../atom/local'
+// import uselocal from '../atom/local'
 
 export default () => {
 
-	const local = uselocal({
-		title: ''
-	})
-	local.title = 'wqew'
+	// const local = uselocal({
+	// 	title: '2222'
+	// })
 
 
 	const source = { uri: 'http://192.168.1.238/soft/node.pdf', cache: false };
 
 	return (
-		<Pdf
-			scale={1}
-			minScale={1.0}
-			maxScale={5.0}
-			horizontal={false}
-			source={source}
-			onLoadComplete={(numberOfPages, filePath) => {
-				Alert.alert(`number of pages: ${numberOfPages}`);
-			}}
-			onPageChanged={(page, numberOfPages) => {
-				Alert.alert(`current page: ${page}`);
-			}}
-			onError={(error) => {
-				Alert.alert(`Error: ${error}`);
+		<View>
+			<Pdf
+				scale={1}
+				minScale={1.0}
+				maxScale={5.0}
+				horizontal={false}
+				source={source}
+				onLoadComplete={(numberOfPages, filePath) => {
+					Alert.alert(`number of pages: ${numberOfPages}`);
+				}}
+				onPageChanged={(page, numberOfPages) => {
+					Alert.alert(`current page: ${page}`);
+				}}
+				onError={(error) => {
+					Alert.alert(`Error: ${error}`);
 
-			}}
-			style={styles.pdf} />
+				}}
+				style={styles.pdf} />
+		</View>
 	);
 }
 
