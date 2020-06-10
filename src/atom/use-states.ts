0e14496fe4ce6b8ec15283b6cc36d8509ec35b97
msz <2473872761@qq.com>
2020-06-10 14:12:01 +0800

    fix useStates

--- src/atom/use-states.ts
+++ src/atom/use-states.ts
@@ -1,33 +1,34 @@
 import { useState } from 'react'
 
-export default function use_states<T>(data: T) {
-	for (const key in data) {
-		// 判断是否实例自身拥有的属性
-		if (data.hasOwnProperty(key)) {
-			const [value, setValue] = useState(data[key]);
-			Object.defineProperty(data, key, {
-				get() {
-					return value
-				},
-				set(v) {
-					setValue(v)
-				}
-			})
+export default function useStates<T extends object>(data: T) {
+	const [local, setLocal] = useState(data);
+	return new Proxy(data, {
+		get(_target: T, key: string | number | symbol) {
+			return local[key];
+		},
+		set(target: T, key: string | number | symbol, value: unknown) {
+			target[key] = value
+			setLocal(target)
+			return true
 		}
-	}
-	return data
+	}) as unknown as T
 }
 
-// export default function useLocal<T>(data: T) {
-// 	const [local, setLocal] = useState(data);
-// 	return {
-// 		...local,
-// 		set: (key: string, value: unknown) => {
-// 			setLocal({
-// 				...local,
-// 				[key]: value
+
+// export default function use_states<T>(data: T) {
+// 	for (const key in data) {
+// 		// 判断是否实例自身拥有的属性
+// 		if (data.hasOwnProperty(key)) {
+// 			const [value, setValue] = useState(data[key]);
+// 			Object.defineProperty(data, key, {
+// 				get() {
+// 					return value
+// 				},
+// 				set(v) {
+// 					setValue(v)
+// 				}
 // 			})
-// 			return local
 // 		}
 // 	}
+// 	return data
 // }
