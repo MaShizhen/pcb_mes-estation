8ebf91987a4fcaa64a7698db75e29ed79afd6a04
msz <2473872761@qq.com>
2020-06-13 18:08:08 +0800

    安灯管理

---
+++ src/atom/message-box.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Alert, Modal, Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
+import Fdicon from './icon';
+
+interface IProp {
+	/**
+	 * 显示
+	 */
+	visible: boolean;
+	/**
+	 * 标题
+	 */
+	title: string;
+	/**
+	 * 附加参数
+	 */
+	args?: object;
+	/**
+	 * 弹唱取消
+	 */
+	toCencel: (args: object) => void;
+	/**
+	 * 弹窗确认
+	 */
+	toConfirm: (args: object) => void;
+	/**
+	 * 组件子节点
+	 */
+	children: Element;
+}
+
+export default (prop: IProp) => {
+
+	return (
+		<Modal
+			animationType={"fade"}
+			transparent={true}
+			visible={prop.visible}
+			onRequestClose={() => {
+				Alert.alert("Modal has been closed.");
+			}}
+		>
+			<TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.5)', flex: 1, flexDirection: 'row', alignItems: 'center' }} onPress={() => {
+				prop.toCencel(prop.args)
+			}}>
+				<TouchableOpacity
+					activeOpacity={1}
+					style={{ backgroundColor: '#fff', width: '30%', marginLeft: '35%', borderRadius: 5 }}>
+					<View style={{ flexDirection: 'row', alignItems: 'center', height: 50, borderBottomWidth: 1, justifyContent: 'space-between', paddingLeft: 15, paddingRight: 15, borderBottomColor: '#999' }}>
+						<Text style={{ fontSize: 18, color: '#333' }}>{prop.title}</Text>
+						<TouchableHighlight
+							style={{ padding: 13 }}
+							onPress={() => prop.toCencel(prop.args)}
+							underlayColor='transparent'>
+							<Fdicon name='guanbi' size={20} color='#333'></Fdicon>
+						</TouchableHighlight>
+					</View>
+					{prop.children}
+					<View style={{ flexDirection: 'row', alignItems: "center", justifyContent: 'space-between', borderTopWidth: 1, borderTopColor: '#f2f2f2' }}>
+						<TouchableOpacity style={{ borderBottomLeftRadius: 5, height: 50, flex: 1 }} onPress={() => prop.toCencel(prop.args)}>
+							<Text style={{ fontSize: 16, textAlign: 'center', lineHeight: 50, color: '#333' }}>取消</Text>
+						</TouchableOpacity>
+						<TouchableOpacity style={{ borderBottomRightRadius: 5, backgroundColor: '#0099ff', height: 50, flex: 1 }} onPress={() => prop.toConfirm(prop.args)}>
+							<Text style={{ fontSize: 16, color: '#fff', textAlign: 'center', lineHeight: 50 }}>确定</Text>
+						</TouchableOpacity>
+					</View>
+				</TouchableOpacity>
+			</TouchableOpacity>
+		</Modal >
+	);
+}
--- src/eandon-system/index.tsx
+++ src/eandon-system/index.tsx
@@ -1,11 +1,12 @@
-import { useFocusEffect } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Picker, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { get_file } from '../atom/config'
 import Fdicon from '../atom/icon';
+import MessageBox from '../atom/message-box'
 import { get } from '../atom/storage'
+import { equipmentlist } from '../home/api';
+import { IEquipmentList } from '../home/interface'
 import { userboard, userboardright } from './api'
-import SetDistribution from './components/set-distribution';
 import { IUserboard, IUserboardRight } from './interface'
 
 interface IProp {
@@ -25,12 +26,20 @@ export default (prop: IProp) => {
 		userboardright: [] as IUserboardRight[],
 		visible: false as boolean
 	})
+	// 弹窗内设备列表
+	const [equipment_list, set_equipment_list] = useState<IEquipmentList[]>([])
+	// 设置左侧弹窗显示
+	const [message_box, set_message_box] = useState({
+		index: 0,
+		args: {}
+	})
+
 	// 初始化查询报警代码列表
 	useEffect(() => {
 		(async () => {
 			const mes_id = await get<string>('mes_id')
 			const userboard_res = await userboard(mes_id)
-			const userboardright_res = await userboardright('mes_id', 'mes_ids')
+			const userboardright_res = await userboardright('74c08b13-aa1e-48fh-a9bc-60257665afa7', '')
 			set_states({
 				...states,
 				userboard: userboard_res.data,
@@ -40,13 +49,48 @@ export default (prop: IProp) => {
 		})()
 	}, []);
 
-	function alertIndex(index: number) {
-		set_states({
-			...states,
-			visible: true
-		})
+	// 头部设备下拉数据
+	useEffect(() => {
+		(async () => {
+			const mes_staff_code = await get<string>('mes_staff_code')
+			const mes_staff_name = await get<string>('mes_staff_name')
+			// const mes_ids = await get<string>('mes_ids')
+			const equipmentlist_res = await equipmentlist(mes_staff_code, mes_staff_name)
+			set_equipment_list(equipmentlist_res.data.sub)
+		})()
+
+	}, []);
+
+	/**
+	 * 下发
+	 */
+	async function issue(args: object) {
+
 	}
 
+	/**
+	 * 移除报警
+	 */
+	async function remove(args: object) {
+
+	}
+
+	/**
+	 * 校验权限
+	 */
+	async function check(args: { _index: number }) {
+		const _index = args._index
+		if (_index === 1) {
+			await issue(args)
+		} else if (_index === 3) {
+			await remove(args)
+		}
+		// 隐藏弹窗
+		set_message_box({
+			index: 0,
+			args: null
+		})
+	}
 
 	return (
 		<ScrollView>
@@ -61,8 +105,8 @@ export default (prop: IProp) => {
 						if (states.userboard.length > 0) {
 							return states.userboard.map((item, index) => {
 								return (
-									<TouchableOpacity onPress={() => alertIndex(index)} >
-										<View key={index} style={{ height: 45, borderWidth: 1, borderRadius: 10, borderColor: '#e2e1de', backgroundColor: '#fff', width: '80%', marginLeft: '10%', marginBottom: 15 }}>
+									<TouchableOpacity key={index} onPress={() => set_message_box({ index: 1, args: item })} >
+										<View style={{ height: 45, borderWidth: 1, borderRadius: 10, borderColor: '#e2e1de', backgroundColor: '#fff', width: '80%', marginLeft: '10%', marginBottom: 15 }}>
 											<Text style={{ lineHeight: 45, textAlign: 'center', fontSize: 16 }}>{item.mes_alarm_name}</Text>
 										</View>
 									</TouchableOpacity>
@@ -83,10 +127,15 @@ export default (prop: IProp) => {
 							if (states.userboardright.length > 0) {
 								return states.userboardright.map((item, index) => {
 									return (
-										<View key={index} style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', height: 120, width: '25%' }}>
+										<TouchableOpacity key={index} style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', height: 120, width: '25%' }} onPress={() => {
+											set_message_box({
+												index: 3,
+												args: item
+											})
+										}}>
 											<Image source={{ uri: get_file + item.mes_alarm_picture }} style={{ borderRadius: 5, width: 120, height: 70, backgroundColor: 'rgba(0,0,0,0.2)' }} />
 											<Text style={{ height: 30, lineHeight: 30, textAlign: 'center', fontSize: 16 }}>{item.mes_alarm_name}</Text>
-										</View>
+										</TouchableOpacity>
 									)
 								})
 							} else {
@@ -98,13 +147,52 @@ export default (prop: IProp) => {
 						})()}
 					</View>
 				</View>
-				<SetDistribution visible={states.visible} id={''} toHide={() => {
-					set_states({
-						...states,
-						visible: false
-					})
-				}} />
 			</View>
+
+
+			<MessageBox title="提交安灯报警" visible={message_box.index === 1} toConfirm={(args) => set_message_box({
+				index: 2,
+				args: {
+					...args,
+					_index: 1
+				}
+			})} toCencel={() => set_message_box({ index: 0, args: null })}>
+				<View style={{ flexDirection: 'row', alignItems: "center", justifyContent: 'center' }}>
+					<Text style={{ fontSize: 16, color: '#333333', textAlign: 'center', lineHeight: 150 }}>可用设备代码：</Text>
+					<Picker
+						style={{ height: 35, width: 250 }} >
+						{
+							equipment_list.map((item, index) => {
+								return <Picker.Item label={item.mes_device_name} key={index} value={item.mes_device_code} />
+							})
+						}
+					</Picker>
+				</View>
+			</MessageBox>
+
+			<MessageBox args={message_box.args} title="验证" visible={message_box.index === 2} toConfirm={(args: { _index: number }) => {
+				check(args)
+			}} toCencel={(args: { _index: number }) => {
+				set_message_box({ index: args._index, args: null })
+			}}>
+				<View style={{ flexDirection: 'row', alignItems: "center", justifyContent: 'center' }}>
+					<Text style={{ fontSize: 16, color: '#333333', textAlign: 'center', lineHeight: 150 }}>请刷卡验证登录</Text>
+				</View>
+			</MessageBox>
+
+			<MessageBox title="提示" visible={message_box.index === 3} toConfirm={(args) => set_message_box({
+				index: 2,
+				args: {
+					...args,
+					_index: 3
+				}
+			})} toCencel={() => set_message_box({ index: 0, args: null })}>
+				<View style={{ flexDirection: 'row', alignItems: "center", justifyContent: 'center' }}>
+					<Text style={{ fontSize: 16, color: '#333333', textAlign: 'center', lineHeight: 150 }}>是否解除安灯报警？</Text>
+				</View>
+			</MessageBox>
+
+
 			{/* 全展示效果 */}
 			{/* <View style={{ flex: 1, paddingLeft: 10, paddingRight: 10, backgroundColor: '#fff', paddingTop: 20 }}>
 				<View style={{ height: 45, backgroundColor: '#fff', marginBottom: 20 }}><Text style={{ lineHeight: 45, textAlign: 'center', fontSize: 18 }}>报警代码详情</Text></View>
